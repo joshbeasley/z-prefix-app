3007f5dd1be98e8973f57189ca8ddfecfc86ef71
Josh Beasley <josh.beasley@hotmail.com>
2022-12-08 21:30:22 -0800

    removed unneeded ui test file

--- ui/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders first react component - the App', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/App is running - good work you who wrote this app:/i);
-  expect(linkElement).toBeInTheDocument();
-});
