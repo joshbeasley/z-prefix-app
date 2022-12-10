53ebd4d2e682ee1fbe61ff09a0ddb21e2b0a92c0
Josh Beasley <josh.beasley@hotmail.com>
2022-12-09 16:02:12 -0800

    final commit hopefully

--- api/src/app.js
+++ api/src/app.js
@@ -21,10 +21,10 @@ app.use(session({
   resave: false,
   name: 'sessionId',
   cookie: {
-    secure: true,
+    secure: process.env.NODE_ENV === 'development' ? false : true,
     httpOnly: true, 
     maxAge: 1000 * 60 * 30, 
-    sameSite: 'none',
+    sameSite: process.env.NODE_ENV === 'development' ? 'lax' : 'none',
   }
 }));
 
